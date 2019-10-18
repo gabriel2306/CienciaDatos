6822d2d968e2de116bbd3a13e859de255f624451
Gabriel Lopez Cuenca <lopezcuenca.gabriel@gmail.com>
2019-10-18 12:44:05 +0200

    plot

--- Practica2/sopImg.R
+++ Practica2/sopImg.R
@@ -1,9 +1,12 @@
 sopImg <- function(data, ruta) {
 
+    install.packages("RColorBrewer")
+    library(RColorBrewer)
+
     png(paste("./tmp/",ruta,sep=""))
 
-    itemFrequencyPlot(data, topN=10, type="relative", 
-        main="Item Frequency")
+    itemFrequencyPlot(data, topN=10, col=brewer.pal(8,'Set1'),
+        type="relative",main="Soporte")
 
     dev.off()
 }
