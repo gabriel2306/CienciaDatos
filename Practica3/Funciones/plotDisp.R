21e24775639b1d0003dbbac6ba246c81d371beb0
Gabriel Lopez Cuenca <lopezcuenca.gabriel@gmail.com>
2019-10-31 13:55:08 +0100

    regresion

--- Practica3/Funciones/plotDisp.R
+++ Practica3/Funciones/plotDisp.R
@@ -1,8 +1,8 @@
-plotDisp <- function(data, regresion, xl, yl, ruta) {
+plotDisp <- function(data, n, m, regresion, xl, yl, ruta) {
 
     png(paste("./tmp/",ruta,sep=""))
 
-    plot(data[,1], data[,2], xlab=xl, ylab=yl,main="Diagrama de Dispersion")
+    plot(data[,n], data[,m], xlab=xl, ylab=yl,main="Diagrama de Dispersion")
     abline(regresion, col="blue")
 
     dev.off()
