01e9568146a6501d3178a39df51d237fdf321a34
Gabriel Lopez Cuenca <lopezcuenca.gabriel@gmail.com>
2019-11-12 20:02:46 +0100

    funcion

--- Practica4/Funciones/KMeans.R
+++ Practica4/Funciones/KMeans.R
@@ -1,4 +1,7 @@
 KMeans <- function(matrizCentroides, matrizMuestras) {
+
+    dimensiones <- length(matrizMuestras[1,])
+
     if (class(matrizCentroides)=="matrix" && class(matrizMuestras)=="matrix") {
 
     } else {
@@ -6,7 +9,7 @@ KMeans <- function(matrizCentroides, matrizMuestras) {
     }
 }
 
-calcularMatrizDistancias <- function (matrizCentroides, matrizMuestras,dimensiones) {
+calcularMatrizDistancias <- function (matrizMuestras, matrizCentroides,dimensiones) {
     sizeCentroides <- length(matrizCentroides)/dimensiones
     sizeMuestras <- length(matrizMuestras)/dimensiones
     x<-c()
@@ -16,7 +19,83 @@ calcularMatrizDistancias <- function (matrizCentroides, matrizMuestras,dimension
             x<-c(x,distanciaEuclidea(matrizCentroides[i,],matrizMuestras[j,]))
     }
 
-    return (matrix(x,nrow=sizeCentroides,ncol=sizeMuestras,byrow=T))
+    m<-matrix(x,nrow=sizeCentroides,ncol=sizeMuestras,byrow=T)
+
+    return(m)
+}
+
+calcularMatrizPertenencia <- function (matrizDistancias, nCentroides) {
+    sizeDistancias <- length(matrizDistancias)/nCentroides
+    x<-c()
+
+    for (i in 1:sizeDistancias) {
+        posMinimo <- filaMinimo(matrizDistancias[,i])
+        for (j in 1:nCentroides)
+            if (j==posMinimo){
+                x<-c(x,1)
+            } else {
+                x<-c(x,0)
+            }
+    }
+
+    m<-matrix(x,nrow=nCentroides,ncol=sizeDistancias)
+
+    return(m)
+}
+
+comprobarMatrizPertenencia <- function (matriz1, matriz2) {
+    iguales<-TRUE
+    i<-1
+
+    while(iguales && i<=length(matriz1)){
+        if (matriz1[i]==matriz2[i]){
+            i<-i+1
+        } else {
+            iguales<-FALSE
+        }
+    }
+
+    return(iguales)
+}
+
+filaMinimo <- function (columnaDistancias) {
+    sizeFilas <- length(columnaDistancias)
+
+    pos<-1
+    menor<-columnaDistancias[1]
+
+    for (i in 1:sizeFilas) {
+        if (columnaDistancias[i] < menor) {
+            menor<-columnaDistancias[i]
+            pos<-i
+        }
+    }
+
+    return(pos)
+}
+
+muestrasPorCluster <- function (matrizPertenencia, nCentroides) {
+    sizeDistancias <- length(matrizPertenencia)/nCentroides
+    finalList<-c()
+    for (i in 1:nCentroides){
+        finalList<-c(finalList, list(muestrasPorFila(matrizPertenencia[i,])))
+    }
+    
+    m<-matrix(finalList,nrow=nCentroides,ncol=sizeDistancias)
+    return(finalList)
+}
+
+muestrasPorFila <- function (filaPertenencia) {
+    sizeFila <- length(filaPertenencia)
+    muestras<-c()
+    
+    for (i in 1:sizeFila){
+        if (filaPertenencia[i]==1){
+            muestras<-c(muestras,i)
+        }
+    }
+
+    return(muestras)
 }
 
 distanciaEuclidea <- function (a,b) {
@@ -27,4 +106,23 @@ distanciaEuclidea <- function (a,b) {
     }
 
     return(sqrt(sum))
+}
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+datosPorCluster <- function (matrizMuestras, muestrasPorCluster, dimensiones) {
+    sizeMuestras <- length(matrizMuestras)/dimensiones
+    colocacion<-c()
+
+    for (i in 1:dimensiones) {
+        mismoCluster<-c()
+        for (j in 1:sizeMuestras) {
+            if (j %in% muestrasPorCluster[i]) {
+                mismoCluster <- c(mismoCluster, list(matrizMuestras[j,]))
+            }
+        }
+        colocacion<-c(colocacion,list(mismoCluster))
+    }
+
+    return(colocacion)
 }
