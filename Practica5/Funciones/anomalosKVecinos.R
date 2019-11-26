435b499bbb1cf48b194532a336bbb339fda79882
Gabriel Lopez Cuenca <lopezcuenca.gabriel@gmail.com>
2019-11-26 21:44:02 +0100

    funcion kvecinos

--- Practica5/Funciones/anomalosKVecinos.R
+++ Practica5/Funciones/anomalosKVecinos.R
@@ -1,10 +1,11 @@
-anomalosKVecinos <- function (data, k, grado) {
+anomalosKVecinos <- function (data, muestra, k, grado) {
     tmp<-""
 
     for(i in 1:5){
         data[,i] = sort(data[,i])
         if(data[k+1,i] > grado) {
-            tmp<-paste(tmp,"La muestra ", i, " es outlier - ",sep="")
+            valor<-paste(muestra[4,1],muestra[4,2])
+            tmp<-paste(tmp,"La muestra ", i, " con valor (", valor, ") es outlier - ",sep="")
         }
     }
 
